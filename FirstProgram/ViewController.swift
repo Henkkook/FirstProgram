13e8c740bb56a034583404c6d5a5b84fecbcb779
D <K>
2022-10-18 12:11:02 +0500

    Initial Commit

---
+++ FirstProgram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstProgram
+//
+//  Created by Denis Kalugin on 18.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
